e0ac11bad4f16a8d5de08cccf7d221c9980b2ed4
niteshbista <nites.bista205@gmail.com>
2024-07-18 19:28:26 +0545

    n

--- src/hw2.java
+++ src/hw2.java
@@ -1,2 +1,42 @@
+import java.util.Scanner;
 public class hw2 {
+    public static void main(String[] args) {
+        int cash;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number ");
+        cash = sc.nextInt();
+
+        if (cash < 100) {
+            int temp= cash/1000;
+            cash= cash%1000;
+            System.out.println("The number of hundred you get back is:" + temp );
+        }
+         if (cash>= 500){
+             int temp= cash/500;
+             cash= cash%500;
+             System.out.println(" the no of the five hundreds you get back is:"+ temp);
+         }
+         if (cash>= 100){
+             int temp= cash/100;
+             cash= cash%100;
+             System.out.println(" the number of hundred you get back is: "+ temp);
+         }
+         if (cash>= 50){
+             int temp= cash/50;
+             cash= cash%50;
+             System.out.println(" the amount of fiftees you get back is"+ temp);
+         }
+         if (cash>= 10){
+             int temp= cash/10;
+             cash= cash%10;
+             System.out.println("the no of tens you get back is:"+ temp);
+         }
+         if (cash>= 5){
+             int temp= cash/5;
+             cash= cash%5;
+             System.out.println("the amount if fives you get back is:"+ temp);
+
+         }
+    }
+
 }
